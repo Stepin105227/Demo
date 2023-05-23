99a62ad05bda05faa630b4dfe61ef9b275fa3bf5
Dushyant_Shekhar_Singh <47522350+Stepin105227@users.noreply.github.com>
2023-05-24 02:42:09 +0530

    Update operator_overloading.cpp

--- FunctionPointer/operator_overloading.cpp
+++ FunctionPointer/operator_overloading.cpp
@@ -1,3 +1,4 @@
+
 // Function pointer on operator overloading
 #include <iostream>
 using namespace std;
@@ -9,10 +10,12 @@ class complex
         void set_data(int a, int b) {x = a; y=b;}
         void display_data()
         {
-            cout<<"Sum is: "<<x<<" + "<<y<<"i" <<"\n";
+            cout<<"Result: "<<x<<" + "<<y<<"i" <<"\n";
         }
         friend complex operator +(complex, complex);
+        friend complex operator -(complex);
 };
+
 complex operator +(complex c1, complex c2)
 {
     complex temp_complex;
@@ -20,11 +23,22 @@ complex operator +(complex c1, complex c2)
     temp_complex.y = c1.y + c2.y;
     return temp_complex;
 }
+
+complex operator -(complex c3)
+{
+    complex temp_complex;
+    temp_complex.x = -c3.x;
+    temp_complex.y = -c3.y;
+    return temp_complex;
+}
+
 int main() {
-    complex comp_obj1,comp_obj2,comp_obj3;
+    complex comp_obj1,comp_obj2,comp_obj3,comp_obj4;
     comp_obj1.set_data(2,3);
     comp_obj2.set_data(4,5);
     comp_obj3 = comp_obj1 + comp_obj2;
     comp_obj3.display_data();
+    comp_obj4 = -comp_obj1;
+    comp_obj4.display_data();
     return 0;
 }
