a88a74e891f66e064c0e1224102dd1a81886e314
Dushyant_Shekhar_Singh <47522350+Stepin105227@users.noreply.github.com>
2023-05-24 01:28:51 +0530

    Create operator_overloading.cpp

---
+++ FunctionPointer/operator_overloading.cpp
@@ -0,0 +1,30 @@
+// Function pointer on operator overloading
+#include <iostream>
+using namespace std;
+class complex
+{
+    private:
+        int x,y;
+    public:
+        void set_data(int a, int b) {x = a; y=b;}
+        void display_data()
+        {
+            cout<<"Sum is: "<<x<<" + "<<y<<"i" <<"\n";
+        }
+        friend complex operator +(complex, complex);
+};
+complex operator +(complex c1, complex c2)
+{
+    complex temp_complex;
+    temp_complex.x = c1.x + c2.x;
+    temp_complex.y = c1.y + c2.y;
+    return temp_complex;
+}
+int main() {
+    complex comp_obj1,comp_obj2,comp_obj3;
+    comp_obj1.set_data(2,3);
+    comp_obj2.set_data(4,5);
+    comp_obj3 = comp_obj1 + comp_obj2;
+    comp_obj3.display_data();
+    return 0;
+}
