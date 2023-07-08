8e53c0eed12fea0434f3507779e2b5bb2345b96c
Dushyant_Shekhar_Singh <47522350+Stepin105227@users.noreply.github.com>
2023-07-08 12:05:48 +0530

    Update SET_CLEAR_BIT.cpp

--- SET_CLEAR_BIT.cpp
+++ SET_CLEAR_BIT.cpp
@@ -3,20 +3,36 @@
 #include <bitset>
 using namespace std;
 
-#define CLEAR(data,pos) (data &= ~(1<<pos))
-#define SET(data,pos) (data |= (1<<pos))
+#define CLEAR(data,pos)   (data &= ~(1<<pos))
+#define SET(data,pos)     (data |= (1<<pos))
+#define TOGGLE(data,pos)  (data ^= (1<<pos))
 
 int main() {
     int data = 0x12;
     bitset<8>y(data);
-    cout<<y<<"\n";
+    cout<<"DATA:    "<<y<<"\n";
     //data |= (1<<2);
     SET(data,2);
     bitset<8>x(data);
-    cout<<x<<"\n";
+    cout<<"SET:     "<<x<<"\n";
     //data &= ~(1<<2);
     CLEAR(data,2);
     bitset<8>m(data);
-    cout<<m<<"\n";
+    cout<<"CLEAR:   "<<m<<"\n";
+    
+    TOGGLE(data,2);
+    bitset<8>n(data);
+    cout<<"TOGGLE:  "<<n<<"\n";
+    TOGGLE(data,2);
+    bitset<8>n1(data);
+    cout<<"TOGGLE:  "<<n1<<"\n";
+    
     return 0;
 }
+/****** output ******
+DATA:    00010010
+SET:     00010110
+CLEAR:   00010010
+TOGGLE:  00010110
+TOGGLE:  00010010
+*********************/
