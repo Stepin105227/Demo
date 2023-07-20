d50d0f9bd5ac684303cc7bbccf2a4336fbbd3949
Dushyant_Shekhar_Singh <47522350+Stepin105227@users.noreply.github.com>
2023-07-21 00:43:36 +0530

    Create auto_datatype.cpp

---
+++ auto_datatype.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stdint.h>
+void add(auto x, auto y);
+void addstring(auto x, auto y);
+
+void addstring(auto *x, auto *y)
+{
+    while(*x == '\0')
+    {
+        std::cout <<"\nSum : "<<(*x+*y);
+    }
+}
+
+void add(auto x, auto y)
+{
+    std::cout <<"\nSum : "<<(x+y);
+}
+int main() {
+    uint8_t a[3] = "AB";
+    uint8_t b[3] = "CD";
+    add(2,3);
+    add(2.5,3);
+    add('A','B');
+    addstring(a,b);
+    return 0;
+}
