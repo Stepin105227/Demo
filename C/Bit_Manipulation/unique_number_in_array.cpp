3b6d16712cb5fa3b48a40957bad42dc0e91942dd
Dushyant_Shekhar_Singh <47522350+Stepin105227@users.noreply.github.com>
2023-09-12 22:48:31 +0530

    Create unique_number_in_array.cpp

---
+++ C/Bit_Manipulation/unique_number_in_array.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+uint8_t find_unique_number(uint8_t *array, uint8_t array_size);
+uint8_t find_unique_number(uint8_t *array, uint8_t array_size)
+{
+    uint8_t result =0;
+    for(uint8_t index =0; index<array_size; index++)
+    {
+        result = result ^ array[index];
+    }
+    return result;
+}
+
+int main()
+{
+    uint8_t array[5] = {5,2,3,2,5};
+    uint8_t array_size = sizeof(array)/sizeof(array[0]);
+    uint8_t unique_number = find_unique_number(array, array_size );
+    printf("%d", unique_number);
+    return 0;
+}
