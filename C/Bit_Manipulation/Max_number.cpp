30a849c3ab5ddcd531955224c71d3653f9067c8f
Dushyant_Shekhar_Singh <47522350+Stepin105227@users.noreply.github.com>
2023-09-14 20:11:41 +0530

    Update Max_number.cpp

--- C/Bit_Manipulation/Max_number.cpp
+++ C/Bit_Manipulation/Max_number.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <stdio.h>
 using namespace std;
@@ -7,18 +6,36 @@ using namespace std;
 int pos_or_neg(int data);
 /* Checking maximum no between two number */
 void max_number(int data1, int data2);
+/* Determine provided two number has opposite signs */
+bool determine_opposite_signs(int data1, int data2);
 
 int main()
 {
     int data = -20;
+    int data1 = 20;
+    int data2 = -80;
     int status = pos_or_neg(data);
-    max_number(20,-80);
+    max_number(data1,data2);
+    bool opposite_sign = determine_opposite_signs(data1,data2);
+    
     if(status == 0)
     printf("\n%d is Even number",data);
     else
     printf("\n%d is Odd number",data);
+    if(opposite_sign == 1)
+    printf("\n%d, %d has opposite sign",data1, data2);
+    else
+    printf("\n%d, %d has same sign",data1, data2);
     return 0;
 }
+
+bool determine_opposite_signs(int data1, int data2)
+{
+    int sign_of_data1 = pos_or_neg(data1);
+    int sign_of_data2 = pos_or_neg(data2);
+    return (sign_of_data1 ^ sign_of_data2);
+}
+
 void max_number(int data1, int data2)
 {
     int diff,result,max_num;
