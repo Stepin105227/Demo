c6465d381a0da9a7ac12cc78a1913ae637ce24df
Dushyant_Shekhar_Singh <47522350+Stepin105227@users.noreply.github.com>
2023-10-11 19:28:53 +0530

    Create Toggle_first_char.c

---
+++ C/String/Toggle_first_char.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+void updated_string(char *str);
+int main() {
+    char str[] = "MY name is dushyant";
+    printf("\nupdated string = %s",str);
+    updated_string(str);
+    return 0;
+}
+void updated_string(char *str)
+{
+    int len = strlen(str);
+    int flag =1;
+    for(int i =0; i<len; i++)
+    {
+        if(str[i] == ' ')
+        {
+            flag =1;
+        }
+        else
+        {
+            if(flag)
+            {
+                if(str[i] >='A' && str[i]<='Z')
+                {
+                  str[i] = str[i] + ('a'-'A');  
+                }
+                else if(str[i] >='a' && str[i]<='z')
+                {
+                    str[i] = str[i] - ('a'-'A');
+                }
+                flag = 0;
+          }
+        }
+    }
+    printf("\nupdated string = %s",str);
+}
