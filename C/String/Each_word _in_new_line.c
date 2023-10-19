a0eab059f5a1a686013afd1568794a9de7f44598
Dushyant_Shekhar_Singh <47522350+Stepin105227@users.noreply.github.com>
2023-10-19 22:24:25 +0530

    Create Each_word _in_new_line.c

---
+++ C/String/Each_word _in_new_line.c
@@ -0,0 +1,25 @@
+// Print each word of the sentence in a new line.
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    char *s;
+    s = malloc(1024 * sizeof(char));
+    scanf("%[^\n]", s);
+    s = realloc(s, strlen(s) + 1);
+    for(int i=0; i<strlen(s);i++)
+    {
+        
+        if(s[i] ==' ')
+        {
+            printf("\n");
+        }
+        else {
+        printf("%c", s[i]);
+        }
+    }
+    return 0;
+}
